6e2d3e74fe2ddb1a9708babff6eb513ff2e3cee8
Raynold Ng <raynold.ng24@gmail.com>
2020-05-12 18:19:59 +0800

    remove dead callback code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {Route, withRouter} from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
 import Question from './Question/Question';
 import Questions from './Questions/Questions';
-import Callback from './Callback';
 import NewQuestion from './NewQuestion/NewQuestion';
 import SecuredRoute from './SecuredRoute/SecuredRoute';
 
@@ -16,10 +15,6 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    if (this.props.location.pathname === '/callback') {
-      this.setState({checkingSession:false});
-      return;
-    }
     this.setState({checkingSession:false});
   }
 
@@ -29,7 +24,6 @@ class App extends Component {
         <NavBar/>
         <Route exact path='/' component={Questions}/>
         <Route exact path='/question/:questionId' component={Question}/>
-        <Route exact path='/callback' component={Callback}/>
         <SecuredRoute path='/new-question'
                       component={NewQuestion}
                       checkingSession={this.state.checkingSession} />
