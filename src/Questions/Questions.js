de6586a9fb29ef35584f7391c3be7e675de5d8e7
Raynold Ng <raynold.ng24@gmail.com>
2020-05-12 18:16:49 +0800

    fix mispelling of contants.js

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -1,8 +1,35 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-import { HOSTNAME } from '../contants'
+import { HOSTNAME } from '../constants'
+
+
+const NewQuestionCard = () => (
+  <Link to="/new-question">
+    <div className="card text-white bg-secondary mb-3">
+      <div className="card-header">Need help? Ask here!</div>
+      <div className="card-body">
+        <h4 className="card-title">+ New Question</h4>
+        <p className="card-text">Don't worry. Help is on the way!</p>
+      </div>
+    </div>
+  </Link>
+)
+
+const QuestionCard = (question) => (
+  <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
+    <Link to={`/question/${question.id}`}>
+      <div className="card text-white bg-success mb-3">
+        <div className="card-header">Answers: {question.answers}</div>
+        <div className="card-body">
+          <h4 className="card-title">{question.title}</h4>
+          <p className="card-text">{question.description}</p>
+        </div>
+      </div>
+    </Link>
+  </div>
+)
 
 class Questions extends Component {
   constructor(props) {
@@ -24,31 +51,11 @@ class Questions extends Component {
     return (
       <div className="container">
         <div className="row">
-          <Link to="/new-question">
-            <div className="card text-white bg-secondary mb-3">
-              <div className="card-header">Need help? Ask here!</div>
-              <div className="card-body">
-                <h4 className="card-title">+ New Question</h4>
-                <p className="card-text">Don't worry. Help is on the way!</p>
-              </div>
-            </div>
-          </Link>
+          {NewQuestionCard()}
           {this.state.questions === null && <p>Loading questions...</p>}
-          {
-            this.state.questions && this.state.questions.map(question => (
-              <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
-                <Link to={`/question/${question.id}`}>
-                  <div className="card text-white bg-success mb-3">
-                    <div className="card-header">Answers: {question.answers}</div>
-                    <div className="card-body">
-                      <h4 className="card-title">{question.title}</h4>
-                      <p className="card-text">{question.description}</p>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            ))
-          }
+          {this.state.questions && this.state.questions.map(question => (
+            QuestionCard(question)
+          ))}
         </div>
       </div>
     )
