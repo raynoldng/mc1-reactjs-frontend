e78e24ededd62db268a8691afbdfefe097ad0347
Raynold Ng <raynold.ng24@gmail.com>
2020-05-13 10:54:42 +0800

    fix hostname path

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // uncomment if using heroku backend
-export const HOSTNAME = "https://protected-fortress-44914.herokuapp.com/"
+export const HOSTNAME = "https://protected-fortress-44914.herokuapp.com"
 
 // uncomment if backend is local
 // export const HOSTNAME = "http://localhost:8081"
