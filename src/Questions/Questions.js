34f36236b7f56af42b8d2dabc4258e63ee0cb7f5
Raynold Ng <raynold.ng24@gmail.com>
2020-05-16 01:58:44 +0800

    quick fix

--- src/Questions/Questions.js
+++ src/Questions/Questions.js
@@ -17,19 +17,42 @@ const NewQuestionCard = () => (
   </Link>
 )
 
-const QuestionCard = (question) => (
-  <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
-    <Link to={`/question/${question.id}`}>
-      <div className="card text-white bg-success mb-3">
-        <div className="card-header">Answers: {question.answers}</div>
-        <div className="card-body">
-          <h4 className="card-title">{question.title}</h4>
-          <p className="card-text">{question.description}</p>
-        </div>
+class QuestionCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: props.question.votes || 0 
+    }
+  }
+
+  render() {
+    const question = this.props.question;
+    return (
+      <div key={question.id} className="col-sm-12 col-md-4 col-lg-3">
+        <Link style={{ textDecoration: 'none' }} to={`/question/${question.id}`}>
+          <div className="card text-white bg-success mb-3">
+            <div className="card-header">Answers: {question.answers}</div>
+            <div className="card-body">
+              <h4 className="card-title">{question.title}</h4>
+              <p className="card-text">{question.description}</p>
+            </div>
+            <div className="card-body">
+              <div className="house-container text-center">
+                <button className="btn btn-danger" onClick={(e) => {
+                  alert('TODO')
+                }}>👍</button>
+                <span className="badge">TODO</span>
+                <button className="btn btn-primary" onClick={(e) => {
+                  alert('TODO')
+                }}>👎</button>
+              </div>
+            </div>
+          </div>
+        </Link>
       </div>
-    </Link>
-  </div>
-)
+    )
+  }
+}
 
 class Questions extends Component {
   constructor(props) {
@@ -54,7 +77,7 @@ class Questions extends Component {
           {NewQuestionCard()}
           {this.state.questions === null && <p>Loading questions...</p>}
           {this.state.questions && this.state.questions.map(question => (
-            QuestionCard(question)
+            <QuestionCard key={question.id} question={question} />
           ))}
         </div>
       </div>
